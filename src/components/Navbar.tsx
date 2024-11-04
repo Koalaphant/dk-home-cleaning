45ce655d6488a68b78c9aaafbda0493f7b14209f
Koalaphant <andrew.wardjones@icloud.com>
2024-11-04 22:13:47 +0000

    improved navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import { HiMenuAlt3 } from "react-icons/hi";
 import { RxCross2 } from "react-icons/rx";
-
 import Link from "next/link";
 import Logo from "./Logo";
 import { usePathname } from "next/navigation";
@@ -19,38 +18,46 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="w-full fixed top-0 left-0 bg-[#105360]">
+    <nav className="w-full fixed top-0 left-0 bg-[#105360] drop-shadow-lg z-50">
       <div className="flex items-center justify-between py-4 md:px-10 px-7">
         <div className="w-[120px]">
-          <Logo fillColor="white" />
+          <Link href="/" onClick={() => setToggle(false)}>
+            <Logo fillColor="white" />
+          </Link>
         </div>
-        <div className="cursor-pointer" onClick={() => setToggle(!toggle)}>
-          {!toggle ? (
+        <div
+          className="cursor-pointer sm:hidden"
+          onClick={() => setToggle(!toggle)}
+        >
+          {toggle ? (
             <RxCross2 className="text-white text-3xl" />
           ) : (
             <HiMenuAlt3 className="text-white text-3xl" />
           )}
         </div>
       </div>
-      <div>
-        {!toggle && (
-          <ul className="flex flex-col items-center mt-5">
-            {links.map((link) => (
-              <li key={link.name} className="w-full text-center">
-                <Link
-                  href={link.href}
-                  className={`block w-full py-5 ${
-                    currentPath === link.href
-                      ? "bg-white text-black"
-                      : "text-white"
-                  }`}
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
+
+      <div
+        className={`absolute top-full left-0 w-full bg-[#105360] transform transition-all duration-300 ease-in-out ${
+          toggle ? "opacity-100 visible" : "opacity-0"
+        } ${!toggle && "delay-100 invisible"} sm:hidden`}
+      >
+        <ul className="flex flex-col items-center">
+          {links.map((link) => (
+            <li key={link.name} className="w-full text-center">
+              <Link
+                href={link.href}
+                className={`block w-full py-5 ${
+                  currentPath === link.href
+                    ? "bg-white text-black"
+                    : "text-white"
+                }`}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
